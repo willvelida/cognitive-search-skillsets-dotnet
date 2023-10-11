19d5ba3b30edb3400fa3fa3819ce923d6fd019f3
Will Velida <willvelida@hotmail.co.uk>
2023-10-11 14:10:51 +1100

    Adding code

---
+++ deploy/main.bicep
@@ -0,0 +1,77 @@
+@description('The suffix applied to deployed resources')
+param suffix string = uniqueString(resourceGroup().id)
+
+@description('The name of the Azure Cognitive Search service that will be deployed')
+param searchServiceName string = 'search-${suffix}-dev'
+
+@description('The name of the Cognitive Service that will be deployed.')
+param cognitiveServiceName string = 'cog-${suffix}-dev'
+
+@description('The pricing tier of the search service that will be deployed. Default is free')
+param searchSku string = 'free'
+
+@description('The SKU that will be applied to the Cognitive Service. Default is S0')
+@allowed([
+  'S0'
+])
+param cognitiveSku string = 'S0'
+
+@description('The location of the Azure Cognitive Search service that will be deployed. Default is location of the resource group')
+param location string = resourceGroup().location
+
+@description('Replicas distribute search workloads across the service.')
+param replicaCount int = 1
+
+@description('Partitions allow for scaling of document count as well as faster indexing by sharding your index over multiple search units')
+param partitionCount int = 1
+
+@description('The name of the Azure Storage account that will be deployed')
+param storageAccountName string = 'storage${suffix}'
+
+@description('The name of the blob container that will be deployed')
+param blobContainerName string = 'cog-search-demo'
+
+@description('The SKU of the Azure Storage account that will be deployed. Default is Standard_LRS')
+param storageAccountSku string = 'Standard_LRS'
+
+resource searchService 'Microsoft.Search/searchServices@2022-09-01' = {
+  name: searchServiceName
+  location: location
+  sku: {
+    name: searchSku
+  }
+  properties: {
+    replicaCount: replicaCount
+    partitionCount: partitionCount
+  }
+}
+
+resource cognitiveSearch 'Microsoft.CognitiveServices/accounts@2023-05-01' = {
+  name: cognitiveServiceName
+  location: location
+  sku: {
+    name: cognitiveSku
+  }
+  kind: 'CognitiveServices'
+  properties: {
+    apiProperties: {
+      statisticsEnabled: false
+    }
+  }
+}
+
+resource storageAccount 'Microsoft.Storage/storageAccounts@2023-01-01' = {
+  name: storageAccountName
+  location: location
+  sku: {
+    name: storageAccountSku
+  }
+  kind: 'StorageV2'
+  properties: {
+    accessTier: 'Hot'
+  }
+}
+
+resource container 'Microsoft.Storage/storageAccounts/blobServices/containers@2023-01-01' = {
+  name: '${storageAccount.name}/default/${blobContainerName}'
+}
---
+++ src/SkillsetsDemo/SkillsetsDemo/Program.cs
@@ -0,0 +1,197 @@
+﻿using Azure;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using Microsoft.Extensions.Configuration;
+using SkillsetsDemo.Models;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false)
+    .Build();
+
+string searchServiceUri = configuration["SearchServiceUri"];
+string adminApiKey = configuration["SearchServiceAdminApiKey"];
+
+SearchIndexClient indexClient = new SearchIndexClient(new Uri(searchServiceUri), new AzureKeyCredential(adminApiKey));
+SearchIndexerClient indexerClient = new SearchIndexerClient(new Uri(searchServiceUri), new AzureKeyCredential(adminApiKey));
+
+// Create a Skillset Pipeline
+
+// Step 1: Create a data source
+SearchIndexerDataSourceConnection dataSource = new SearchIndexerDataSourceConnection(
+    name: "demodata",
+    type: SearchIndexerDataSourceType.AzureBlob,
+    connectionString: configuration["AzureBlobConnectionString"],
+    container: new SearchIndexerDataContainer("cog-search-demo"))
+{
+    Description = "Demo files to demonstrate cognitive search capabilities"
+};
+
+try
+{
+    await indexerClient.CreateOrUpdateDataSourceConnectionAsync(dataSource);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+    throw;
+}
+
+// Step 2: Create a skillset
+List<InputFieldMappingEntry> inputFieldMappingEntries = new List<InputFieldMappingEntry>();
+inputFieldMappingEntries.Add(new InputFieldMappingEntry("text")
+{
+    Source = "/document/pages/*"
+});
+inputFieldMappingEntries.Add(new InputFieldMappingEntry("languageCode")
+{
+    Source = "/document/languageCode"
+});
+
+List<OutputFieldMappingEntry> outputFieldMappingEntries = new List<OutputFieldMappingEntry>();
+outputFieldMappingEntries.Add(new OutputFieldMappingEntry("keyPhrases")
+{
+    TargetName = "keyPhrases"
+});
+
+KeyPhraseExtractionSkill keyPhraseExtractionSkill = new KeyPhraseExtractionSkill(inputFieldMappingEntries, outputFieldMappingEntries)
+{
+    Description = "Extract the key phrases",
+    Context = "/document/pages/*",
+    DefaultLanguageCode = KeyPhraseExtractionSkillLanguage.En
+};
+
+List<SearchIndexerSkill> skills = new List<SearchIndexerSkill>();
+skills.Add(keyPhraseExtractionSkill);
+
+// Creating a skillset using the built in skillset (not specifying an AI services key)
+SearchIndexerSkillset skillset = new SearchIndexerSkillset("demoskillset", skills)
+{
+    Description = "Demo skillset"
+};
+
+try
+{
+    await indexerClient.CreateOrUpdateSkillsetAsync(skillset);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception thrown: {ex.Message}");
+    throw;
+}
+
+// Step 3: Create an index
+FieldBuilder builder = new FieldBuilder();
+var index = new SearchIndex("demoindex")
+{
+    Fields = builder.Build(typeof(DemoIndex))
+};
+
+try
+{
+    indexClient.GetIndex(index.Name);
+    indexClient.DeleteIndex(index.Name);
+}
+catch (RequestFailedException ex) when (ex.Status == 404)
+{
+    // throw 404
+}
+
+try
+{
+    indexClient.CreateIndex(index);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception thrown: {ex.Message}");
+    throw;
+}
+
+// Step 4: Create and run an indexer
+IndexingParameters parameters = new IndexingParameters()
+{
+    MaxFailedItems = -1,
+    MaxFailedItemsPerBatch = -1,
+    IndexingParametersConfiguration = new IndexingParametersConfiguration()
+};
+parameters.IndexingParametersConfiguration.Add("dataToExtract", "contentAndMetadata");
+parameters.IndexingParametersConfiguration.Add("imageAction", "generateNormalizedImages");
+
+SearchIndexer indexer = new SearchIndexer("demoindexer", dataSource.Name, index.Name)
+{
+    Description = "Demo indexer",
+    SkillsetName = skillset.Name,
+    Parameters = parameters
+};
+
+FieldMappingFunction mappingFunction = new FieldMappingFunction("base64Encode");
+mappingFunction.Parameters.Add("useHttpServerUtilityUrlTokenEncode", true);
+
+indexer.FieldMappings.Add(new FieldMapping("metadata_storage_path")
+{
+    TargetFieldName = "id",
+    MappingFunction = mappingFunction
+
+});
+indexer.FieldMappings.Add(new FieldMapping("content")
+{
+    TargetFieldName = "content"
+});
+
+indexer.OutputFieldMappings.Add(new FieldMapping("/document/pages/*/organizations/*")
+{
+    TargetFieldName = "organizations"
+});
+indexer.OutputFieldMappings.Add(new FieldMapping("/document/pages/*/keyPhrases/*")
+{
+    TargetFieldName = "keyPhrases"
+});
+indexer.OutputFieldMappings.Add(new FieldMapping("/document/languageCode")
+{
+    TargetFieldName = "languageCode"
+});
+
+try
+{
+    indexerClient.GetIndexer(indexer.Name);
+    indexerClient.DeleteIndexer(indexer.Name);
+}
+catch (RequestFailedException ex) when (ex.Status == 404)
+{
+    //if the specified indexer not exist, 404 will be thrown.
+}
+
+try
+{
+    indexerClient.CreateIndexer(indexer);
+}
+catch (RequestFailedException ex)
+{
+    Console.WriteLine("Failed to create the indexer\n Exception message: {0}\n", ex.Message);
+}
+
+// Step 4: Monitor Indexing
+try
+{
+    var demoIndexerExecutionInfo = indexerClient.GetIndexerStatus(indexer.Name);
+
+    switch (demoIndexerExecutionInfo.Value.Status)
+    {
+        case IndexerStatus.Unknown:
+            Console.WriteLine("The status is unknown!");
+            break;
+        case IndexerStatus.Error:
+            Console.WriteLine("There's an error. Check the portal");
+            break;
+        case IndexerStatus.Running:
+            Console.WriteLine("The Indexer is running!");
+            break;
+        default:
+            Console.WriteLine("No information available");
+            break;
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception thrown: {ex.Message}");
+    throw;
+}
